53f8f5d5a84b7318d7395c19d9dd0054703bbd3d
胡鹏 <15151873042@163.com>
2023-12-25 17:50:40 +0800

    http和nacos方式注册

---
+++ springcloud-client/src/main/java/com/nbb/shenyu/springcloudclient/TestController.java
@@ -0,0 +1,20 @@
+package com.nbb.shenyu.springcloudclient;
+
+import org.apache.shenyu.client.springcloud.annotation.ShenyuSpringCloudClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ShenyuSpringCloudClient("/test/**") // 该注解修饰的接口会被注册到shenyu-admin
+@RequestMapping("/test")
+public class TestController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/index")
+    public String echo(String string) {
+        return port;
+    }
+}
